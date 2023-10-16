a8de8570366022ff4ed7b609af4446002d970c4c
Trinds <carlos.bem.t0123933@edu.atec.pt>
2023-10-16 20:27:52 +0100

    created functional list and create pet form

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,70 @@
-import { useEffect } from "react"
-import { createOrUpdatePet, deletePet, getAllPets, getPetById } from "./services/main/pets"
+import {useEffect, useState} from "react"
+import {createOrUpdatePet, deletePet, getAllPets, getPetById} from "./services/main/pets"
 
 function App() {
-  useEffect(() => {
-    //Permitir cancelar um pedido ao servidor
-    const abortController = new AbortController();
+    const [petList, setPetList] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [hasError, setHasError] = useState(false)
 
-    async function test() {
-      const data = {
-        name: "Bobby",
-        dateOfBirth: "2019-01-01",
-        breed: "Bulldog",
-      }
+    useEffect(() => {
+        //Permitir cancelar um pedido ao servidor
+        const abortController = new AbortController();
 
-      const petCreated = await createOrUpdatePet(data)
-      
-      console.log(petCreated)
+        try {
+            getAllPets().then((data) => {
+                setPetList(data)
+            })
+        } catch (error) {
+            setHasError(true)
+        } finally {
+            setIsLoading(false)
+        }
 
-      const allPets = await getAllPets()
+        return () => {
+            //Cancelar o pedido caso o componente seja desmontado
+            abortController.abort();
+        };
+    }, [])
 
-      console.log(allPets)
+    function handleFormSubmit(e) {
+        e.preventDefault()
 
-      petCreated.name = "Fisher"
-      const petUpdated = await createOrUpdatePet(petCreated)
-
-      console.log(petUpdated)
-
-      const pet = await getPetById(petUpdated.id)
-
-      console.log(pet)
-
-      await deletePet(petUpdated.id)
-
-      const allPetsAfterDelete = await getAllPets()
-
-      console.log(allPetsAfterDelete)
-
+        try {
+            createOrUpdatePet({
+                name: e.target.querySelector('#petNameInput').value,
+                breed: e.target.querySelector('#petBreedInput').value,
+                dateOfBirth: e.target.querySelector('#petDobInput').value
+            })
+        } catch (error) {
+            console.log('Not possible to create pet rn, try later.')
+        }
     }
 
-    test()
+    return (
+        <>
+            <h1>Pet Care - Carlos do Bem</h1>
+            <button onClick={() => showCreatePetForm}>Create Pet</button>
+            {isLoading && <h6>Loading...</h6>}
+            {hasError && <h6>Something went wrong. Try again later.</h6>}
+            <ul>
+                {petList.map((pet) => {
+                    const {id, name, breed, dateOfBirth} = pet
+                    return <li key={id}>
+                        <p>Name: {name}</p>
+                        <p>Breed: {breed}</p>
+                        <p>Date Of Birth: {dateOfBirth}</p>
+                    </li>
+                })}
+            </ul>
 
-    return () => {
-      //Cancelar o pedido caso o componente seja desmontado
-      abortController.abort();
-    };
-  }, [])
-
-
-  return (
-    <>
-      <h1>Base project</h1>
-    </>
-  )
+            <form onSubmit={() => handleFormSubmit(event)}>
+                <input id="petNameInput" placeholder="Name" type="text" defaultValue=""/>
+                <input id="petBreedInput" placeholder="Breed" type="text" defaultValue=""/>
+                <input id="petDobInput" placeholder="Date of Birth" type="date" defaultValue=""/>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
 }
 
 export default App
